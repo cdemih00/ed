210901fa7bddee475df8ff03f44f8272f3b66b58
cdemih00 <cdemih00@estudiantes.unileon.es>
2025-03-06 16:40:52 +0100

    ed06/03/25pract1

--- Practica1_2025_paraEclipse/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImpl.java
+++ Practica1_2025_paraEclipse/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImpl.java
@@ -27,6 +27,7 @@ public class RestaurantArrayImpl implements IRestaurant {
 		this.maxCapacity=aforoMax;
 		this.discount=discount;
 		this.nClients=0;
+		this.nTables=nTables;
 		
 	}
 
@@ -52,7 +53,9 @@ public class RestaurantArrayImpl implements IRestaurant {
 		int contadorChildren=0;
 		for(int i=0;i<this.nTables;i++){
 			
+			if(tables[i]!=null) {
 			contadorChildren+=tables[i].getNChildren();
+			}
 		}
 		
 		return contadorChildren;
@@ -154,7 +157,10 @@ public class RestaurantArrayImpl implements IRestaurant {
 	public double getFinalPriceRestaurant() {
 		double precio=0;
 		for(int i=0;i<this.tables.length;i++) {
-			precio+=this.tables[i].getTotalService();
+			if(this.tables[i]!=null) {
+				precio+=this.tables[i].getTotalService();
+			}
+			
 		}
 		return precio;
     }
@@ -163,21 +169,57 @@ public class RestaurantArrayImpl implements IRestaurant {
 
 	@Override
 	public boolean emptyTable(int nTable) {
-		
-		return false;
+		boolean vaciada=false;
+		if (this.tables[nTable - 1] != null) {
+	        this.nClients -= this.tables[nTable - 1].getNChildren() + this.tables[nTable - 1].getOrder().size();
+	        this.tables[nTable - 1] = null; // Libera la mesa
+	        vaciada = true;
+	    }
+	    return vaciada;
 	}
 
 
 
 	@Override
 	public int occupyTable(int nPeople, int nChildren) {
-		return 0;
-	}
+
+		if (this.maxCapacity < this.nClients + nPeople + nChildren) {
+	        return -1; // No hay suficiente capacidad
+	    }
+
+	    boolean encontrado = false;
+	    int i = 0;
+
+	    while (i < this.nTables && !encontrado) {
+	        if (this.tables[i] == null) {
+	            this.tables[i] = new Service(nPeople, nChildren); // Asigna el servicio a la mesa
+	            this.nClients += nPeople + nChildren; // Actualiza la cantidad de clientes
+	            encontrado = true;
+	            return i + 1; // Retorna el número de mesa (1-based index)
+	        }
+	        i++;
+	    }
+
+	    return -2; // No hay mesas disponibles
+	}	
 	
 	@Override
 	public boolean occupyTable(int nTable, int nPeople, int nChildren) {
-		return false;
 		
+		
+		if (this.maxCapacity < this.nClients + nPeople + nChildren) {
+	        return false;
+	    }
+	    if (this.tables[nTable - 1] != null) { 
+	        return false;
+	    }
+	    
+	    this.tables[nTable - 1] = new Service(nPeople, nChildren);
+	    this.nClients += nPeople + nChildren;
+	    
+	    return true;
+	    
 	}
-	
+		
 }
+	
